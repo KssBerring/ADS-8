4d4848cc3d685dbd36ddb47e83cb8e55a7097c7f
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-05-12 20:37:52 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,34 +1,41 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-Train::Train(){countOp = 0;
-    first = nullptr;}
+
+Train::Train() {
+    first = nullptr;
+    countOp = 0;
+}
 
 void Train::addCage(bool light) {
-        Cage* newCage = new Cage{ light, nullptr, nullptr };
-        if (!first) {
-            first = newCage;
-            newCage->next = newCage;
-            newCage->prev = newCage;
-        }
-        else {
-            newCage->next = first;
-            newCage->prev = first->prev;
-            first->prev->next = newCage;
-            first->prev = newCage;
-        }
+    Cage *newCage = new Cage;
+    newCage->light = light;
+    if (first == nullptr) {
+        first = newCage;
+        newCage->next = newCage;
+        newCage->prev = newCage;
+    } else {
+        newCage->next = first;
+        newCage->prev = first->prev;
+        first->prev->next = newCage;
+        first->prev = newCage;
     }
+}
 
 int Train::getLength() {
-        if (!first) return 0;
-        Cage* current = first;
-        int length = 1;
-        while (current->next != first) {
-            length++;
-            current = current->next;
-        }
-        return length;
+    if (first == nullptr) {
+        return 0;
     }
 
+    Cage *current = first->next;
+    int length = 1;
+    while (current != first) {
+        length++;
+        current = current->next;
+    }
+
+    return length;
+}
+
 int Train::getOpCount() {
-  return countOp;
+    return countOp;
 }
