78cf88d6f322fba287a67ab3b865f0341e28e4af
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-05-12 20:24:55 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -12,33 +12,9 @@ class Train {
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Cage *first; // точка входа в поезд (первый вагон)
  public:
-  Train(): countOp(0), first(nullptr) {}
-  void addCage(bool light){
-        Cage* newCage = new Cage{ light, nullptr, nullptr };
-        if (!first) {
-            first = newCage;
-            newCage->next = newCage;
-            newCage->prev = newCage;
-        }
-        else {
-            newCage->next = first;
-            newCage->prev = first->prev;
-            first->prev->next = newCage;
-            first->prev = newCage;
-        }
-    } // добавить вагон с начальным состоянием лампочки
-  int getLength(){
-        if (!first) return 0;
-        Cage* current = first;
-        int length = 1;
-        while (current->next != first) {
-            length++;
-            current = current->next;
-        }
-        return length;
-    }        // вычислить длину поезда
-  int getOpCount(){
-        return countOp;
-    }         // вернуть число переходов (из вагона в вагон)
+  Train();
+  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
+  int getLength();       // вычислить длину поезда
+  int getOpCount(); // вернуть число переходов (из вагона в вагон)
 };
 #endif  // INCLUDE_TRAIN_H_
